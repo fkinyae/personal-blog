ab7c4b643dbc9801f0add1e1622c3a21a2df5c57
Francis Kinyae <fmkinyae@gmail.com>
2021-08-26 12:00:51 +0300

    add comment form and view

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User
+from ..models import User,Role,Blog
 from wtforms import ValidationError
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
+    
+    
+class BlogForm(FlaskForm):
+    title = StringField('Blog title',validators=[Required()])
+    brief = TextAreaField('Story Intro')  
+    description = TextAreaField('Full Story')
+    submit = SubmitField('Submit')
+    
+class CommentForm(FlaskForm):
+
+ comment = TextAreaField('Add Your Comment')
+
+ submit = SubmitField('Submit')    
+        
     
 
         
--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,47 @@
-from flask import render_template,request,url_for,abort,redirect
+from flask import render_template,request,url_for,abort,redirect,flash
 from . import main
 from flask_login import login_required, current_user
-from ..models import  User,Role
-from .forms import UpdateProfile
+from ..models import  User,Role,Blog,Comments
+from .forms import UpdateProfile,BlogForm,CommentForm
 from .. import db,photos
 import markdown2
+from ..request import get_quotes, repeat_get_quotes
 
 
-@main.route('/')
+@main.route('/', methods = ['GET','POST'])
 def index():
-
     
-    return render_template('index.html')
+    blogs = Blog.query.all()
+    quote = get_quotes
+    quotes = repeat_get_quotes(1, get_quotes)
+        
+    return render_template('blog.html',blogs=blogs,quotes=quotes)
+        
+@main.route('/blog/new', methods = ['GET','POST'])
+@login_required
+def new_blog():
+    form = BlogForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        brief = form.brief.data
+        description = form.description.data
+        
+        new_blog = Blog(title=title,brief=brief,description=description,user=current_user)
+        
+        new_blog.save_blog()  
+        return redirect(url_for('main.index'))
+    
+    return render_template('new_blog.html',blog_form=form)   
+
+@main.route('/blog/single/<int:id>', methods = ['GET','POST'])
+@login_required
+def single_blog(id):
+    
+    id = Blog.query.filter_by().first().id
+    
+    blogs = Blog. get_blog(id)
+        
+    return render_template('single_blog.html',blogs=blogs,id=id)
 
 
 @main.route('/user/<uname>')
@@ -53,3 +83,51 @@ def update_pic(uname):
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))    
 
+@main.route('/blog/comment/new/<int:id>', methods=['GET','POST'])
+@login_required
+def post_comment(id):
+    form = CommentForm()
+    blog = Blog.query.filter_by(id=id).first().id
+    if form.validate_on_submit():
+        comment=form.comment.data
+        blog_id = Blog.query.filter_by(id=id).first().id
+
+        
+        new_comment=Comments(comment=comment,blog_id=blog_id,user=current_user)
+        db.session.add(new_comment)
+        db.session.commit()
+        
+        return redirect(url_for('main.index'))
+        
+    title = 'New Comment'    
+    return render_template('all_comments.html',title=title, comment_form=form,blog=blog)
+
+@main.route('/comment/<int:id>')
+def single_comment(id):
+    comment=Comments.query.get(id)
+    if comment is not None:
+        abort(404)
+    format_comment=markdown2.markdown(comment.comment,extras=["code-friendly", "fenced-code-blocks"])   
+    return render_template('comment.html',comment = comment,format_comment=format_comment) 
+
+@main.route('/all_comments/<int:id>', methods=['GET','POST'])
+def all_comments(id):
+    form = CommentForm()
+    blog_id = Blog.query.filter_by(id=id).first().id
+    comments=Comments.get_comments(blog_id)
+    if form.validate_on_submit():
+        comment=form.comment.data
+        blog_id = Blog.query.filter_by(id=id).first().id
+
+        
+        new_comment=Comments(comment=comment,blog_id=blog_id,user=current_user)
+        db.session.add(new_comment)
+        db.session.commit()
+    
+    
+    return render_template('all_comments.html',blog_id=blog_id,comments=comments,comment_form=form)
+
+
+
+    
+    
